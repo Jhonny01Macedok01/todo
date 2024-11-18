882888a07321190aefa1666d587dfd692051bb5a
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-18 16:55:46 -0400

    created class of exceptions error ObjNotFound, ExceptionHandler and StandarError

---
+++ src/main/java/com/jhonny/todo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.jhonny.todo.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jhonny.todo.services.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.ServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, ServletRequest request) {
+
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
--- src/main/java/com/jhonny/todo/services/TodoService.java
+++ src/main/java/com/jhonny/todo/services/TodoService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jhonny.todo.domain.Todo;
 import com.jhonny.todo.repositories.TodoRepository;
+import com.jhonny.todo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TodoService {
@@ -17,7 +18,8 @@ public class TodoService {
 	
 	public Todo findById(Integer id) {
 		Optional<Todo> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Todo.class.getName()));
 	}
 
 	public List<Todo> findAllOpen() {
