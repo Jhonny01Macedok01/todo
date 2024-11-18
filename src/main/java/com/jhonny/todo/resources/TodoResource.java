c380ad5e9d06bcf5d791dcbd5146153b2f97740e
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-18 14:23:32 -0400

    implementd methods CREATED and DELETE

--- src/main/java/com/jhonny/todo/resources/TodoResource.java
+++ src/main/java/com/jhonny/todo/resources/TodoResource.java
@@ -1,13 +1,18 @@
 package com.jhonny.todo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jhonny.todo.domain.Todo;
 import com.jhonny.todo.services.TodoService;
@@ -41,5 +46,19 @@ public class TodoResource {
 	public ResponseEntity<List<Todo>> listAll() {
 		List<Todo> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Todo> create(@RequestBody Todo obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity <Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
